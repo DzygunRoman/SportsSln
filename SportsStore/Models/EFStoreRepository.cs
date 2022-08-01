7d50ef5951848acd476ef4421da681ff6486652e
Roman Dzygun <dzygun-roman@mail.ru>
2022-08-01 13:38:48 +0300

    SportsSln

---
+++ SportsStore/Models/EFStoreRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+
+namespace SportsStore.Models
+{
+    public class EFStoreRepository:IStoreRepository//Патерн "Хранилище"
+    {
+        private StoreDbContext context;
+        public EFStoreRepository(StoreDbContext ctx)
+        {
+            context = ctx;
+        }
+        public IQueryable<Product>Products=>context.Products;
+    }
+}
--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -7,14 +7,24 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using SportsStore.Models;
 
 namespace SportsStore
 {
     public class Startup
     {
+        public Startup(IConfiguration config)
+        {
+            Configuration = config;
+        }
+        private IConfiguration Configuration { get; set; }//Предоставляет доступ в систему конфигурации ASP.NET Core
         public void ConfigureServices(IServiceCollection services)//Применяется для настройки служб
         {
             services.AddControllersWithViews();//Настраивает совместно используемые объекты, требующиеся в приложениях, которые эксплуатируют MVC Framework механизм визуализации Razor
+            services.AddDbContext<StoreDbContext>(opts => { opts.UseSqlServer(Configuration["ConnectionStrings:SportsStoreConnection"]); });//Регистрирует класс контекста базы данных и конфигурирует связь с базой данных
+            services.AddScoped<IStoreRepository, EFStoreRepository>();//Создает службу, в которой каждый HTTP-запрос получает собственный объект хранилища
         }
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
