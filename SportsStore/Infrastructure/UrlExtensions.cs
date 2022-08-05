f2fd8a3b92ee90b42f77c33a971d7b1171a93379
Roman Dzygun <dzygun-roman@mail.ru>
2022-08-05 10:59:51 +0300

    SportsSln

---
+++ SportsStore/Infrastructure/UrlExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace SportsStore.Infrastructure
+{
+    public static class UrlExtensions
+    {
+        //Генерирует URL, по которому бразер будет возвращаться после обновления корзины, принимая во внимание строку запроса, если она есть
+        public static string PathAndQuery(this HttpRequest request) => request.QueryString.HasValue ? $"{request.QueryString}" : request.Path.ToString();
+    }
+}
